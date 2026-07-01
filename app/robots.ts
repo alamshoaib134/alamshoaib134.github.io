6f232622c5e16c3454867e0acbf16d8b7fc4c1e8
Shoaib Alam <alamshoaib134@gmail.com>
2026-07-02 01:57:38 +0530

    feat: explicitly permit AI crawlers in robots.txt configuration

--- app/robots.ts
+++ app/robots.ts
@@ -4,10 +4,28 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = "https://alamshoaib134.github.io";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
